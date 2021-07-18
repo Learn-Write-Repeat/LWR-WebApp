64a70800d20963823b1b664ac8ea5752c4c9b6ef
sanyogchangmai <sanyogchangmai29@gmail.com>
2021-07-18 10:40:21 +0530

    Added express rest api

---
+++ backend/models/blog.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema({
+    title:{
+        type: String,
+    },
+    description:{
+        type: String,
+    },
+    body:{
+        type: String,
+    },
+    thumbnail:{
+        type: String,
+    }
+},{timestamps: true});
+
+const Blog = mongoose.model("Blog",blogSchema);
+
+module.exports = Blog;
